53998d76aa71a8f4a5d64ac44254e640bae91544
not-JASH <jluna094@uottawa.ca>
2022-10-18 14:58:09 -0400

    created spyder project

---
+++ Netowks/Generator.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 20:11:19 2022
+
+@author: zbot1
+"""
+
+#%%
+class Generator:
+"""
+        
+"""
+    def __init__(self,layersizes):
+        
+
+
+#%%
+"""
+        Forward Pass Functions
+"""
+    def predict(self,X):
+        return Y
+
+    def scalinglayer(self,X):
+       return Y
+   
+    def ODDTlayer(self,X):
+        return Y
+    
+    def waveletlayer(self,X):
+        return Y
+    
+    def setencodinglayer(self,X):
+        return Y
+    
+    def estimateencodinglayer(self,X):
+        return Y
+    
+    def predictionlayer(self,X):
+        return Y
+
+#%%
+"""
+        Initialization Functions
+"""
+    def init_scalinglayer(layersizes):
+        # initializes scaling layer
+        
+    def init_inversescalinglayer(layersizes):
+        # initialize inverse scaling layer
+        
+    def init_ODDTlayer(layersizes):
+        # initializes outlier detection and detrend layer
+        
+    def init_waveletlayer(layersize):
+        # initializes wavelet layer (might not be required)
+        
+    def init_setencodinglayer(layersizes):
+        # initializes set encoding layer
+        
+    def init_estimateencodinglayer(layersizes):
+        # initializes estimate encoding layer
+        
+    def init_predictionlayer(layersizes):
+        # initializes prediction layer
+        
+        
