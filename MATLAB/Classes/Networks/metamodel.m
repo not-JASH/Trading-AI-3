d0f02181420785a3a03f9d45482e0dfb89b93140
not-JASH <jluna094@uottawa.ca>
2022-11-07 09:21:55 -0500

    started metamodel

--- MATLAB/Classes/Networks/metamodel.m
+++ MATLAB/Classes/Networks/metamodel.m
@@ -11,6 +11,31 @@ classdef metamodel < DeepNetwork
     end
 
     methods
+        function obj = metamodel()
+
+
+        end
+
+        function layersizes = generator(obj)
+
+
+
+            function ls = reweight_detrend()
+
+            end
+
+            function ls = set_encoding()
+
+            end
+
+            function ls = estimate_encoding()
+
+            end
+        end
+
+        function layersizes = discriminator(obj)
+
+        end
 
     end
 end
